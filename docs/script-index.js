8882ee72f58e05649ecad6b8235b1f7071a094fd
Franco <114626590+elfrancoide@users.noreply.github.com>
2023-08-14 17:48:10 -0300

    Update script-index.js

--- docs/script-index.js
+++ docs/script-index.js
@@ -17,7 +17,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 
     if (validarLogin()){
         this.submit()
-        window.location.href = "../index.html";
+        window.location.href = "/index.html";
     }
 })
 
