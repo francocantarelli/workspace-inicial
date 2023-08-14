bf502b0438a5ccb4799e7ec1019657d187c730c5
Franco <21.cantarelli@gmail.com>
2023-08-14 14:42:15 -0300

    Implementacion del login

---
+++ script-index.js
@@ -0,0 +1,27 @@
+function alertError(){
+    alert("La contraseña debe ser mayor a 6 caracteres")
+}
+
+function validarLogin(){
+    const contraseña = document.getElementById("contraseña").value.trim()
+
+    if (contraseña.length < 6){
+        alertError();
+        return false;
+    }
+    return true;
+}
+
+document.getElementById("login-form").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    if (validarLogin()){
+        this.submit()
+        window.location.href = "index.html";
+    }
+})
+
+
+
+
+
